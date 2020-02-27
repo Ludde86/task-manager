1ee8bb4dc9573320114ffb4a04d78cffc2efeda9
Ludde86 <bslbjorn@gmail.com>
2020-02-27 12:34:28 +0100

    add TaskList component which renders our list

---
+++ src/components/TaskList.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { TaskListContext } from '../context/TaskListContext';
+
+// useContext allows us to bring (return) in the current context value
+// to use that context we must import that context itself
+
+// this functional component will render the state (data) as a list (the array created in TaskListContext),
+// -> for that we will use the useContext
+const TaskList = () => {
+	// we can use destructuring and get access to the tasks from the state
+	const { tasks } = useContext(TaskListContext);
+
+	// in order to render list on the page we have to loop (map) through the array
+	// -> we will use javascript {} to return all items within this array
+	// we will create a new component for each task (this component is the whole list of tasks)
+	return (
+		<div>
+			<ul className="list">
+				{tasks.map((task) => {
+					return <Task />;
+				})}
+			</ul>
+		</div>
+	);
+};
+
+export default TaskList;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-// render the App component -> select the root development (id root from index.html)
+// render the App component -> select the root div element (id root from index.html)
 ReactDOM.render(<App />, document.getElementById('root'));
