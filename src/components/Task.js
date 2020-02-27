cc2658722a6974b511699c511e0e2e10fa95bd7e
Ludde86 <bslbjorn@gmail.com>
2020-02-27 12:42:10 +0100

    add Task component which will renders each task

---
+++ src/components/Task.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// this functional component will renders each task from the state (tasks array)
+// each task will contain the title of the task,
+// -> and two buttons
+const Task = () => {
+	return <li className="list-item">Task</li>;
+};
+
+export default Task;
