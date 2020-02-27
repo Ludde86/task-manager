e4a5198f4e31506a11ff87aab9ec4f7efbe54688
Ludde86 <bslbjorn@gmail.com>
2020-02-27 12:47:08 +0100

    add functionality in Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,7 +4,17 @@ import React from 'react';
 // each task will contain the title of the task,
 // -> and two buttons
 const Task = () => {
-	return <li className="list-item">Task</li>;
+	return (
+		<li className="list-item">
+			<span>Task Title</span>
+			<button className="btn-delete task-btn">
+				<i className="fas fa-trash-alt" />
+			</button>
+			<button className="btn-edit task-btn">
+				<i className="fas fa-pen" />
+			</button>
+		</li>
+	);
 };
 
 export default Task;
