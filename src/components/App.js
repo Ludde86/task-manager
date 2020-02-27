bc4641063c434534c09b68c12c3e346771975c1b
Ludde86 <bslbjorn@gmail.com>
2020-02-27 12:16:04 +0100

    add state in TaskListContextProvider

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import '../App.css';
 
 // functional component
 const App = () => {
+	// create state -> insert task into the state (manually) -> render the tasks on the page
+
 	return <div>App</div>;
 };
 
--- src/context/TaskListContext.js
+++ src/context/TaskListContext.js
@@ -7,10 +7,19 @@ import React, { useState, createContext } from 'react';
 export const TaskListContext = createContext();
 
 // create a component which will include the state (as a convention we use the name of the context followed by provider)
+// this component will then renders the state, as a list (this list we will create in another component)
 const TaskListContextProvider = () => {
 	// useState accepts an argument which is the initial state
 	// we set the initial state as an array with with three objects
-	useState([ { task: 'Read the book', id: 1 }, { task: 'Wash the car', id: 2 }, { task: 'Write some code', id: 3 } ]);
+	// useState returns an array with two values
+	// -> the actual data (the array of these objecs)
+	// -> the function which we use to edit or update the state (data)
+	// in order to bring these values we have to use an array destructuring
+	const [ tasks, setTasks ] = useState([
+		{ task: 'Read the book', id: 1 },
+		{ task: 'Wash the car', id: 2 },
+		{ task: 'Write some code', id: 3 }
+	]);
 
 	return <div>Task List Context</div>;
 };
