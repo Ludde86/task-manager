31cc674dcbfceb17ac2083586ec6197c1080d489
Ludde86 <bslbjorn@gmail.com>
2020-02-27 13:14:10 +0100

    fix div element to buttons in Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -7,12 +7,14 @@ const Task = () => {
 	return (
 		<li className="list-item">
 			<span>Task Title</span>
-			<button className="btn-delete task-btn">
-				<i className="fas fa-trash-alt" />
-			</button>
-			<button className="btn-edit task-btn">
-				<i className="fas fa-pen" />
-			</button>
+			<div>
+				<button className="btn-delete task-btn">
+					<i className="fas fa-trash-alt" />
+				</button>
+				<button className="btn-edit task-btn">
+					<i className="fas fa-pen" />
+				</button>
+			</div>
 		</li>
 	);
 };
